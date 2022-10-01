9d98ef8d59cd3340dd10bd564ab4d07f976e2ff1
betilla x <aclara_77@yahoo.com>
2022-10-01 22:25:26 +0100

    cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: [process.env.ORIGIN, 'http://localhost:3000'];
+    origin: [process.env.ORIGIN, 'http://localhost:3000']
 }));
 
 app.get('/:id', function (req, res) {
